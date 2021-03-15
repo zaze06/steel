7a5a014d7e4a3a5e8286a15ff239a450175eb7c1
zaze06 <54169874+zaze06@users.noreply.github.com>
2021-03-15 17:59:36 +0100

    adding steel arrmor

--- src/main/java/mod/alien/steel/Main.java
+++ src/main/java/mod/alien/steel/Main.java
@@ -1,5 +1,6 @@
 package mod.alien.steel;
 
+import net.minecraft.resources.DataPackRegistries;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
